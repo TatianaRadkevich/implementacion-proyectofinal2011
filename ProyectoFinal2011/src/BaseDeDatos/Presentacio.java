02523b18bb2fc393ce5200d353d1bc2d6e98e448
ivan2551 <ivan2551@ce03fc2c-e59f-5848-eacc-e44a3eab4920>
2011-06-04 21:07:25 +0000

    Prueba, se sube proyecto java

---
+++ ProyectoFinal2011/src/BaseDeDatos/Presentacio.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Presentacion;
+
+/**
+ *
+ * @author Ivan
+ */
+public class Presentacio {
+
+}
