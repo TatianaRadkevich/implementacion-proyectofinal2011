fd6480953038e263e63560204e05e12f7e021dea
ivan2551 <ivan2551@ce03fc2c-e59f-5848-eacc-e44a3eab4920>
2011-10-25 04:00:22 +0000

    Estad

--- ProyectoFinal2011/src/Negocio/Produccion/EstadoOrdenTrabajo.java
+++ ProyectoFinal2011/src/Negocio/Produccion/EstadoOrdenTrabajo.java
@@ -8,6 +8,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -44,7 +45,7 @@ public class EstadoOrdenTrabajo  implements java.io.Serializable {
     }
    
      @Id 
-    
+    @GeneratedValue
     @Column(name="ID_EORDEN_TRABAJO", unique=true, nullable=false, precision=2, scale=0)
     public byte getIdEordenTrabajo() {
         return this.idEordenTrabajo;
